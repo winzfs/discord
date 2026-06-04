78ddc8e82ab983d2912b0d0390a1e37f8f71a4ad
winzfs <jazzhjm@gmail.com>
2026-06-04 20:10:34 +0900

    Wire admin summary to D1 service

--- apps/api/src/routes/admin.routes.ts
+++ apps/api/src/routes/admin.routes.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
 import { adminRequired } from "../middleware/adminRequired";
+import { getAdminSummary } from "../services/adminSummaryService";
 import type { AppEnv } from "../utils/env";
 import { ok } from "../utils/response";
 
 export const adminRoutes = new Hono<AppEnv>();
 
-adminRoutes.get("/summary", adminRequired, (c) => ok(c, { recentRuns: [], adminLogs: [] }));
+adminRoutes.get("/summary", adminRequired, async (c) => ok(c, await getAdminSummary(c.env.DB)));
