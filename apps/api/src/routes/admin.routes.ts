62406c08c25f22187b04ad19c2726fc9a4c4724b
winzfs <jazzhjm@gmail.com>
2026-06-04 20:06:43 +0900

    Type admin routes with AppEnv

--- apps/api/src/routes/admin.routes.ts
+++ apps/api/src/routes/admin.routes.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
 import { adminRequired } from "../middleware/adminRequired";
+import type { AppEnv } from "../utils/env";
 import { ok } from "../utils/response";
 
-export const adminRoutes = new Hono();
+export const adminRoutes = new Hono<AppEnv>();
 
 adminRoutes.get("/summary", adminRequired, (c) => ok(c, { recentRuns: [], adminLogs: [] }));
