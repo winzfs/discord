4a853e8e5aec272a43a42714e2a96b0eb961331d
winzfs <jazzhjm@gmail.com>
2026-06-07 00:31:01 +0900

    Import pass reward screen styles

--- apps/web/src/pages/LobbyPage.tsx
+++ apps/web/src/pages/LobbyPage.tsx
@@ -25,6 +25,7 @@ import { defaultLobbyLineupSize, loadLobbyProgress, saveLobbyProgress } from "..
 import type { LobbyAccountProgress } from "../game-lobby/lobbyAccountProgress";
 import "../styles/lobby.css";
 import "../styles/lobby-polish.css";
+import "../styles/lobby-pass.css";
 import "../styles/lobby-recruit-reveal.css";
 import "../styles/lobby-stage-polish.css";
 
