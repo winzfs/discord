07b70aca4447b2f5ec837134e6a6e1aa05832701
winzfs <jazzhjm@gmail.com>
2026-07-14 07:50:12 +0900

    feat: open Reaction Lab exclusively in embedded activity mode

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
+import { isEmbeddedActivity } from "../lib/activityMode";
+import { ReactionLabPage } from "../pages/ReactionLabPage";
 import { AppProviders } from "./providers";
 import { router } from "./router";
 
 export function App() {
   return (
     <AppProviders>
-      <RouterProvider router={router} />
+      {isEmbeddedActivity() ? <ReactionLabPage activityMode /> : <RouterProvider router={router} />}
     </AppProviders>
   );
 }
